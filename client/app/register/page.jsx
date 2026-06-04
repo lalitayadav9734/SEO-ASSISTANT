0179477ec15c893520b9be0645b5e0bf37864c01
Lalita Yadav <166160645+lalitayadav9734@users.noreply.github.com>
2026-06-04 23:22:26 +0530

    changes

--- client/app/register/page.jsx
+++ client/app/register/page.jsx
@@ -4,7 +4,10 @@ const[name,setName] = useState('');
   return(
     <>
     <h1>Create Your Account</h1>
-    <input type="text" placeholder="Type Your Name" value={name} onChange={(e) => SetName(e.target.value)}/>
+    <input type="text" placeholder="Type Your Name" value={name} onChange={(e) => setName(e.target.value)}/>
+    <input type="text" placeholder="Type Your Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+    <input type="password" placeholder="Type Your Password" value={password} onChange={(e) => setPassword(e.target.value)}/> 
+    <button onClick={}>Register</button>
     </>
   )
 }
